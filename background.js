69af4f40708ece572ec30867fe88532dd723ab7e
Nicholas <nicholas@nftstory.life>
2021-07-21 02:41:54 -0400

    Updated encoding

--- background.js
+++ background.js
@@ -3,28 +3,32 @@
 const apikey = "USERNAME:APITOKEN";
 
 function addPin() {
-  getCurrentTab().then(tab => {
-    post(tab.url, tab.title)
-  });
+    getCurrentTab().then(tab => {
+        post(tab.url, tab.title)
+    });
 }
 
 async function getCurrentTab() {
-  let queryOptions = { active: true, currentWindow: true };
-  let [tab] = await chrome.tabs.query(queryOptions);
-  return tab;
+    let queryOptions = { active: true, currentWindow: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
 }
 
 async function post(url, title) {
-  fetch('https://api.pinboard.in/v1/posts/add' + '?auth_token=' + apikey + '&url=' + encodeURI(url) + '&description=' + encodeURI(title ? title.substring(0,200) : 'title'))
-    .then(async response => {
-      try {
-        const data = await response
-        console.log(response);
-        console.log('response data ', data)
-      } catch (error) { 
-        console.log('error: '); console.error(error) 
-      }
-    })
+    title = title ? title.substring(0, 200) : 'title'
+    title = encodeURI(title);
+    url = encodeURI(url);
+    console.log(title);
+    fetch('https://api.pinboard.in/v1/posts/add' + '?auth_token=' + apikey +  '&description=' + title+'&url=' + url)
+        .then(async response => {
+            try {
+                const data = await response
+                console.log(response);
+                console.log('response data ', data)
+            } catch (error) {
+                console.log('error: '); console.error(error)
+            }
+        })
     // .then(data => { console.log(data) })
 }
 
